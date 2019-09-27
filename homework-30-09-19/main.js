033d836d2146b6077c33dcb114cf0837447f8135
Katherine <47138336+rocka840@users.noreply.github.com>
2019-09-27 11:14:38 +0200

    Update main.js

--- homework-30-09-19/main.js
+++ homework-30-09-19/main.js
@@ -1,3 +1,3 @@
 var first = prompt("What is your First Name?")
 var family = prompt("What is your Family Name?")
-alert("Welcome" + first + " "+ family + ", this is your first JS class.")
+alert("Welcome" + first + " " + family + ", this is your first JS class.")
